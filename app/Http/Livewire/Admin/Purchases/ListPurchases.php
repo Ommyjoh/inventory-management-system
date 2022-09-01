5d26ddb6b0ccaf9590f36d338dafd00dba60ced5
Ommyjoh <emmanuelboshe@gmail.com>
2022-09-01 03:59:31 +0300

    changes

--- app/Http/Livewire/Admin/Purchases/ListPurchases.php
+++ app/Http/Livewire/Admin/Purchases/ListPurchases.php
@@ -7,8 +7,9 @@ use Livewire\Component;
 
 class ListPurchases extends Component
 {
-    public $listeners = ['approve' => 'approvePurchase'];
+    public $listeners = ['approve' => 'approvePurchase', 'deleted' => 'deletePurchase'];
     public $approvedId;
+    public $deleteId;
 
     public function approvePurchaseAlert($id){
 
@@ -22,7 +23,21 @@ class ListPurchases extends Component
         $purchase->status = "APPROVED";
         $purchase->save();
 
-        $this->dispatchBrowserEvent('approvalSuccessModal', ['message'=>'Purchase approved successful!']);
+        $this->dispatchBrowserEvent('approvalSuccessModal', ['message'=>'Purchase Approved Successful!']);
+    }
+
+    public function deleteAlert($id){
+
+        $this->deleteId = $id;
+        $this->dispatchBrowserEvent('deleteConfirmation');
+    }
+
+    public function deletePurchase(){
+
+        $purchase  = Purchase::findOrFail($this->deleteId);
+        
+        $purchase->delete();
+        $this->dispatchBrowserEvent('deletedSuccessModal', ['message' => 'Purchase Deleted Successfully!']);
     }
 
     public function render()
