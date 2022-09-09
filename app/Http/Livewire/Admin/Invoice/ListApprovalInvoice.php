f51257337a224255707c59900d5b6565232fec82
Ommyjoh <emmanuelboshe@gmail.com>
2022-09-09 05:01:25 +0300

    changes

---
+++ app/Http/Livewire/Admin/Invoice/ListApprovalInvoice.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Invoice;
+
+use Livewire\Component;
+
+class ListApprovalInvoice extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.invoice.list-approval-invoice');
+    }
+}
